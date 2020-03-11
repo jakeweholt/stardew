d59a1158cd5141913af0947f1d2f312c1d68c0cd
jakew <jakew@remitly.com>
2020-03-10 20:09:24 -0700

    adding api, tests, and moving data into python app

--- croptimizer/api.py
+++ croptimizer/api.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/', methods=['POST'])
+def post():
+    if request.method == 'POST':
+        return jsonify(request.json)
+
+# curl --url http://127.0.0.1:5000/ -X POST -H "Content-Type: application/json" --data '{"oid":"12341234"}'
