6f80a1e93067fe4c965826574e8f19bdf39b857b
Rick Steele <psteele@adobe.com>
2020-03-10 21:11:56 -0700

    updated mock data, server replies with mock response, visiting in browser gives curl helptext

--- croptimizer/api.py
+++ croptimizer/api.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
+import json
 app = Flask(__name__)
 
+with open('./mock/mockOutput.json') as file:
+  mockOutput = json.load(file);
 
 @app.route('/', methods=['POST'])
 def post():
     if request.method == 'POST':
-        return jsonify(request.json)
+        return mockOutput
 
-# curl --url http://127.0.0.1:5000/ -X POST -H "Content-Type: application/json" --data '{"oid":"12341234"}'
+@app.route('/', methods=['GET'])
+def get():
+    if request.method == 'GET':
+        testResponse = "curl --url http://127.0.0.1:5000/ -X POST -H \"Content-Type: application/json\" --data '{\"foo\":\"bar\"}'"
+        return testResponse
